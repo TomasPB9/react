d21771bee81fbb35e06e1c5d6206242e52fb040e
Tomas Bolio <tomas_9724@hotmail.com>
2021-06-11 13:12:20 -0500

    examples use let and const

---
+++ src/basis/let-const.js
@@ -0,0 +1,15 @@
+// let y const
+const name = 'Carlos';
+const lastName = 'Posada';
+
+let valor = 5;
+valor = 6;
+
+console.log(name, lastName, valor);
+
+if(true){
+  const name = 'Tomas';
+  console.log(name);
+}
+
+console.log(valor);
